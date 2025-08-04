42ec5cda8b7145d4d9874023a4aa4815af19050f
Varunkmr481 <kmrvarun481@gmail.com>
2025-08-04 22:22:50 +0530

    add Rank page with user leaderboard; implement API call to fetch users and display in Leaderboard component

--- Client/src/pages/Rank.jsx
+++ Client/src/pages/Rank.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import Leaderboard from "../components/LeaderBoard";
 
 const Rank = () => {
-  return <div>Coming soon..</div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function getAllUsers() {
+      // 1. Get the token
+      const token = localStorage.getItem("token");
+
+      // 2. send http request
+      const res = await fetch("http://localhost:8000/getAllUsers", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: token,
+        },
+      });
+
+      // 3. Get the res
+      const data = await res.json();
+
+      console.log(data.users);
+      setUsers(data.users);
+    }
+
+    getAllUsers();
+  }, []);
+
+  return <Leaderboard users={users}></Leaderboard>;
 };
 
 export default Rank;
